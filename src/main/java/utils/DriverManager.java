a67713b66c7aeed89049769cc1f2f980c3298472
Ahmed Fawzy <117578070+Ahmed-Fawzy-Tester@users.noreply.github.com>
2026-03-27 17:28:50 +0200

    Update DriverManager.java

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -13,9 +13,15 @@ public class DriverManager {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--remote-allow-origins=*");
             options.addArguments("--disable-blink-features=AutomationControlled");
+
+            // مهم جدًا لجينكنز/لينكس
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+
             driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
         }
         return driver;
     }
